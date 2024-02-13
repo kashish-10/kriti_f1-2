6ad8775f3838c65785025c05654823162eaeb19a
kashish <ks1998743@gmail.com>
2024-02-14 01:46:49 +0530

    first commit

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,8 +1,114 @@
 import 'package:diva/emergency/emergencyCarousel.dart';
 import 'package:diva/nearbySafePlaces/NearbySafePlaces.dart';
+import 'package:shake/shake.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:background_sms/background_sms.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePage createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  Position? _currentPosition;
+  String? _currentAddress;
+  LocationPermission? permission;
+  _getPermission() async => await [Permission.sms].request();
+  _isPermissionGranted() async => await Permission.sms.status.isGranted;
+  _sendSms(String phoneNumber, String message, {int? simSlot}) async {
+    SmsStatus result = await BackgroundSms.sendMessage(
+        phoneNumber: phoneNumber, message: message, simSlot: 1);
+    if (result == SmsStatus.sent) {
+      print("Sent");
+      Fluttertoast.showToast(msg: "send");
+    } else {
+      Fluttertoast.showToast(msg: "failed");
+    }
+  }
+
+  _getCurrentLocation() async {
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      Fluttertoast.showToast(msg: "Location permission are denied");
+      if (permission == LocationPermission.deniedForever) {
+        await Geolocator.requestPermission();
+        Fluttertoast.showToast(
+            msg: "Location permissions are permanently denied");
+      }
+    }
+    Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high,
+            forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+        print(_currentPosition!.latitude);
+        _getAddressFromLatLon();
+      });
+    }).catchError((e) {
+      Fluttertoast.showToast(msg: e.toString());
+    });
+  }
+
+  _getAddressFromLatLon() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          _currentPosition!.latitude, _currentPosition!.longitude);
+      Placemark place = placemarks[0];
+      setState(() {
+        _currentAddress =
+            "${place.locality},${place.postalCode},${place.street},";
+      });
+    } catch (e) {
+      Fluttertoast.showToast(msg: e.toString());
+    }
+  }
+
+  getAndSendSms() async {
+    String recipients = "8278681942";
+    // List<TContact> contactList = await DatabaseHelper().getContactList();
+    // String messageBody =
+    //     "https://maps.google.com/?daddr=${_currentPosition!.latitude},${_currentPosition!.longitude}";
+    String messageBody = "https://maps.google.com/?daddr=25.7821353,84.7102497";
+    if (await _isPermissionGranted()) {
+      // contactList.forEach((element) {
+      //   _sendSms("${element.number}", "I am in trouble $messageBody");
+      // });
+      _sendSms(recipients, "I am in trouble $messageBody");
+    } else {
+      Fluttertoast.showToast(msg: "Something is wrong. Please try again.");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getPermission();
+    _getCurrentLocation();
+    ShakeDetector.autoStart(
+      onPhoneShake: () {
+        getAndSendSms();
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Shake!'),
+          ),
+        );
+        // Do stuff on phone shake
+      },
+      minimumShakeCount: 5,
+      shakeSlopTimeMS: 500,
+      shakeCountResetTime: 3000,
+      shakeThresholdGravity: 2.7,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,6 +138,17 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               NearbySafePlaces(),
+              SizedBox(height: 20), // Adding some space between the widgets
+              ElevatedButton(
+                onPressed: () {
+                  // Define the action you want to perform when the button is tapped
+                  // For now, it just shows a toast message
+                  // _getCurrentLocation();
+                  getAndSendSms();
+                  // Fluttertoast.showToast(msg: 'Button tapped!');
+                },
+                child: Text('Tap Me'),
+              ),
             ],
           )),
     );
